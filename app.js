398507c059dcbdeb5212d05f5ed8c9542da4a2c7
Oscar <oscarvesga@gmail.com>
2019-05-06 22:15:36 -0500

    version 1

--- app.js
+++ app.js
@@ -1,21 +1,86 @@
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    methodOverride = require("method-override");
-    mongoose = require("mongoose");
+'use strict'
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(methodOverride());
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-var router = express.Router();
+const Product = require('./models/products')
 
-router.get('/', (req, res) => {
-    res.send("Hola mundo");
-});
+const app = express();
+const port = process.env.PORT || 3000
 
-app.use(router);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.listen(3000, () => {
-    console.log("Node server running on http://localhost:3000");
-});
+app.get('/api/products', (req, res) => {
+    Product.find({}, (err, products) => {
+        if (err) return res.status(500).send({message: `GET Error petition: ${err}`})
+        if (!products) return res.status(404).send({message: 'Products not find'})
+
+        res.status(200).send({ products });  
+    })
+})
+
+app.get('/api/products/:productId', (req, res) => {
+    let productId = req.params.productId
+
+    Product.findById(productId, (err, product) => {
+        if (err) return res.status(500).send({message: `GET Error petition: ${err}`})
+        if (!product) return res.status(404).send({message: 'Product not find'})
+
+        res.status(200).send({product});
+    })
+})
+
+app.post('/api/products', (req, res) => {
+    console.log('POST /api/products')
+    console.log(req.body)
+
+    let product = new Product()
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category = req.body.category
+    product.description = req.body.description
+
+    product.save((err, productStored) => {
+        if (err) res.status(500).send({message: `POST Error: ${err}`})
+
+        res.status(200).send({product: productStored})
+    })
+})
+
+app.put('/api/products/:productId', (req, res) => {
+    let productId = req.params.productId
+    let update = req.body
+
+    Product.findByIdAndUpdate(productId, update, (err, productUpdate) => {
+        if (err) res.status(500).send({message: `PUT Error: ${err}`})
+
+        res.status(200).send({ product: productUpdate })
+    })
+})
+
+app.delete('/api/products/:productId', (req, res) => {
+    let productId = req.params.productId
+
+    Product.findById(productId, (err, product) => {
+        if (err) res.status(500).send({message: `DELETE Error: ${err}`})
+
+        product.remove(err => {
+            if (err) res.status(500).send({message: `DELETE error: ${err}`})
+            res.status(200).send({message: `El producto ha sido eliminado`})
+        })
+    })
+})
+
+mongoose.connect('mongodb://localhost:27017/shop', { useNewUrlParser: true }, (err, res) => {
+    if(err) {
+        return console.log(`DB connect error ${err}`)
+    }
+    console.log('DB connect success')
+
+    app.listen(port, () => {
+        console.log(`Node server running on http://localhost:${port}`)
+    })
+})
