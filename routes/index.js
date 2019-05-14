c11c2b85594c9b7f885260a57a7d9598f04eed4b
Oscar <oscarvesga@gmail.com>
2019-05-13 20:39:57 -0500

    v2 auth middleware

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,18 @@
 const express = require('express')
 const api = express.Router()
 const productCtrl = require('../controllers/products')
+const userCtrl = require('../controllers/users')
+const auth = require('../middleware/auth')
 
 api.get('/products', productCtrl.getProducts)
 api.get('/products/:productId', productCtrl.getProduct)
-api.post('/products', productCtrl.saveProduct)
-api.put('/products/:productId', productCtrl.updateProduct)
-api.delete('/products/:productId', productCtrl.deleteProduct)
+api.post('/products', auth, productCtrl.saveProduct)
+api.put('/products/:productId', auth, productCtrl.updateProduct)
+api.delete('/products/:productId', auth, productCtrl.deleteProduct)
+api.post('/signup', userCtrl.signUp)
+api.post('/signin', userCtrl.signIn)
+api.get('/private', auth, (req, res) => {
+    res.status(200).send({ message: `Access allowed` })
+})
 
 module.exports = api
